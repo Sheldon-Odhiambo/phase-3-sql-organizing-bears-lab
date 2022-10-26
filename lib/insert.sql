73a43fd341168df3bf8616f5d6902d2d71d4d3bc
JUdihouma04 <sheldonodhiuma04@gmail.com>
2022-10-27 01:37:55 +0300

    still debugging

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1,"Mr. Chocolate");
+INSERT INTO bears VALUES (2,"Rowdy");
+INSERT INTO bears VALUES (3,"Tabitha",);
+INSERT INTO bears VALUES (4,"Sargeant Brown");
+INSERT INTO bears VALUES (5,"Melissa";
+INSERT INTO bears VALUES (6,"Grinch");
+INSERT INTO bears VALUES (7,"Wendy");
+INSERT INTO bears VALUES (8,"nul");
